e457330f87103d7af5c90570f4b73d724b596d03
Aman <amankumarharsh889@gmail.com>
2024-07-03 19:53:26 +0530

    max

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
+import streamlit as st
+import yfinance as yf
+
 from keras.models import load_model 
 import streamlit as st
 import matplotlib.pyplot as plt
